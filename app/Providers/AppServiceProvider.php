136249fe71dafae1717f34e0fde1c16e8187099f
SereyVathna <s.s.vathna@gmail.com>
2019-01-10 16:28:25 +0700

    config Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,10 @@
+use Illuminate\Support\Facades\Schema;
 <?php
 
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**

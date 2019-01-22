a9f8044385fb80f81d977490d5ce45594f996213
VirakyuthPK <virakyuth.pk@gmail.com>
2019-01-22 16:08:37 +0700

    change home_page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     //
+    public function __construct() {
+      return $this->middleware('auth');
+    }
+    public function create() {
+      return view('post');
+    }
+    public function store(Request $request) {
+      
+    }
 }

60d8ef698c354d5b1c2ac46cfaa610ac31881aa7
SereyVathna <s.s.vathna@gmail.com>
2019-01-22 16:26:20 +0700

    index in Book Controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Book;
 
 class BookController extends Controller
 {
@@ -13,7 +14,7 @@ class BookController extends Controller
      */
     public function index()
     {
-        
+        return Book::all();
     }
 
     /**

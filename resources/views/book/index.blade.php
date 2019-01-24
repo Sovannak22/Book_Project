e654f64acdc4b3e4da6062b5f59fe41479aadfa0
SereyVathna <s.s.vathna@gmail.com>
2019-01-24 14:55:26 +0700

    ot dg

---
+++ resources/views/book/index.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row">
+    <div class="col-sm-2 bg-danger">
+        <h1>Hi</h1>
+    </div>
+    <div class="col bg-warning">
+        <h1>Hello</h1>
+        <div class="row">
+            <div class="col-sm-10 bg-light">
+                <div class="row">
+                    <h1 class="col-sm-9">Book Store</h1>
+                    <div class="dropdown show  font-weight-bold mt-auto">
+                        Sorb by
+                        <a class="btn btn-sm btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Interested
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <a class="dropdown-item" href="#">Interested</a>
+                            <a class="dropdown-item" href="#">Date</a>
+                            <a class="dropdown-item" href="#">Name</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-5">
+                    <div>
+                        Example1
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-3 my-3 ">
+                            <div class="card" style="width: 10rem;">
+                                <img class="card-img-top img-card " src="img/b1.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+
+                                </div>
+                                <div class="card-footer">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3 my-3 ">
+                            <div class="card" style="width: 10rem;">
+                                <img class="card-img-top img-card " src="img/Web 1920 – 1.png" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+
+                                </div>
+                                <div class="card-footer">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3 my-3 ">
+                            <div class="card" style="width: 10rem;">
+                                <img class="card-img-top img-card " src="img/Web 1920 – 1.png" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+
+                                </div>
+                                <div class="card-footer">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3 my-3 ">
+                            <div class="card" style="width: 10rem;">
+                                <img class="card-img-top img-card " src="img/Web 1920 – 1.png" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+
+                                </div>
+                                <div class="card-footer">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3 my-3 ">
+                            <div class="card" style="width: 10rem;">
+                                <img class="card-img-top img-card " src="img/Web 1920 – 1.png" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+
+                                </div>
+                                <div class="card-footer">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3 my-3 ">
+                            <div class="card" style="width: 10rem;">
+                                <img class="card-img-top img-card " src="img/Web 1920 – 1.png" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+
+                                </div>
+                                <div class="card-footer">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection

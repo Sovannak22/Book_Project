25916dc8025ae7d69a1cf8ddd6d5a864fd32a80a
DARA <pondara69@gmail.com>
2019-02-23 23:33:23 +0700

    lala

--- resources/views/profile/CreateProfile.blade.php
+++ resources/views/profile/CreateProfile.blade.php
@@ -90,7 +90,7 @@
 	<!-- midle content=============================================================== -->
 
 	<!-- store======================================================================= -->
-	<div class="container-fluid">
+	<!-- <div class="container-fluid">
 		<div class="row">
 			<div class="col-2">
 				
@@ -118,9 +118,78 @@
 				
 			</div>
 		</div>
-	</div>
+	</div> -->
 	<!-- feed=========================================================================== -->
-
+	<div>
+	@foreach ($posts as $post)
+            <div class="d-flex justify-content-center">
+              <div class="post-status col-10">
+                <div class="post">
+                  <div class="row">
+                    <div class="col">
+                      <div class="user-btn">
+                        <a href="#">
+                          <div class="row">
+                            <img src="/images/cats_blue_eyes_animals_pets_4288x2848.jpg" class="user-img-btn" alt=" ">
+                            <p class="user-name-btn">
+                              {{ $post->user->name }}
+                            </p>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                    <div class="col-3">
+                      @if (Auth::user()->id != $post->user->id)
+                        <button type="button" class="follow-btn btn btn-primary" name="button-follow">
+                          + follow
+                        </button>
+                      @endif
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col status-post">
+                      <p class="status-text">
+                        {{ $post->description }}
+                      </p>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <div class="row like-count">
+                        <div class="">
+                            <i class="fas fa-thumbs-up"></i>
+                        </div>
+                        <div class="like-num">
+                          <p>1.2 k</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-2">
+                      <p>{{ $comment->where('post_id', $post->id)->count() }}
+                        @if ($comment->where('post_id', $post->id)->count() <= 1)
+                          comment
+                        @else
+                          comments
+                        @endif
+                      </p>
+                    </div>
+                  </div>
+                  <div class="row cmt-share-btn-group">
+                    <div class="col">
+                      <a class="like-btn btn btn-secondary" href="{{ route('feeds.like', $post->id) }}">like</a>
+                    </div>
+                    <div class="col">
+                      <a class="comment-btn btn btn-secondary" href="{{ route('feeds.show', $post->id) }}">comment</a>
+                    </div>
+                    <div class="col">
+                      <a class="share-btn btn btn-secondary">share</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          @endforeach
+	</div>
 	<!-- card=========================================================================== -->
 <!-- follower alert===================================================================== -->
 <div class="modal fade" id="followerButton" tabindex="-1" role="dialog" aria-labelledby="followerButton" aria-hidden="true" style="margin-top: 5%;">

7a06683fc3cdaea0548021b621ab82ee05f2a61d
Sovannak22 <38850319+Sovannak22@users.noreply.github.com>
2019-02-25 21:31:21 +0700

    fix dropdown

--- resources/views/etc/header.blade.php
+++ resources/views/etc/header.blade.php
@@ -1,10 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark navbar-laravel">
     <div class="container ">
-        <!-- <a class="navbar-brand  text-success" href="{{ url('/') }}">
-            {{ config('app.name', 'Book Project') }}
-        </a> -->
         <a class="navbar-brand  text-success" href="{{ url('/') }}">
-            <!-- {{ config('app.name', 'Book Project') }} -->
             Book Project
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -36,15 +32,10 @@
                     <li class="nav-item dropdown">
 
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            <img class="round-img" height="35px" width="35px" style="margin-right: 5px;" src="images/{{Auth::user() ->profile_img}}"
-                                    alt="">
-                            
-                            <span class="caret" style="font-size: 15pt;padding-top: 5px;"><b>{{ Auth::user()->name }}</b></span>
+                            {{ Auth::user()->name }}
                         </a>
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('profile.index') }}">
-                                {{ __('Profile') }}
-                            </a>
+                            <a href="/profile" class="dropdown-item">Profile</a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">
@@ -58,22 +49,8 @@
                     </li>
                     <li class="nav-item">
                         <a href="/books" class="nav-link mt-1" style="">
-                            <div class="" style="height:35px;border-left: solid #8c8c8c 0.1rem;border-right: solid #8c8c8c 0.1rem;padding-top: -1px;">
+                            <div class="">
                                 <p style="margin-left: 5px;margin-right: 5px;padding-top: 1px;font-size:12pt;"><b>Book Store</b></p>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="" class="nav-link" style="">
-                            <div class="">
-                                <img width="25rem" height="25rem" src="{{asset('icon/gear.svg')}}" alt="">
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="" class="nav-link" style="">
-                            <div class="">
-                                <img width="25rem" height="25rem" src="{{asset('icon/question-mark.svg')}}" alt="">
                             </div>
                         </a>
                     </li>

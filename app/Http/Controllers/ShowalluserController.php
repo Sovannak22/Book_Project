004619ece9b9eb85f13cfab5b3bf2bdd697a63e4
DARA <pondara69@gmail.com>
2019-02-27 00:31:39 +0700

    toto

--- app/Http/Controllers/ShowalluserController.php
+++ app/Http/Controllers/ShowalluserController.php
@@ -27,11 +27,17 @@ class ShowalluserController extends Controller
     }
     public function follow($id)
     {
-       $follow = DB::table('follows');
-       $follow->user_id=$id;
-       $follow->follower_id=Auth::user()->id;
-       dd($follow);
-       $follow->save();
+
+		$books = DB::table('follow')
+            ->join('users','user_id','follows.user_id')
+            ->where('user_id',$user_id)
+            ->get();
+
+       // $follow = DB::table('follows');
+       // $follow->user_id=$id;
+       // $follow->follower_id=Auth::user()->id;
+       // dd($follow);
+       // $follow->save();
        return view('profile.CreateProfile');
     }
 }
